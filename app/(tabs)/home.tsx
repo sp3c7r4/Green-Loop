1ff89efe3d1e1d8dfc1dd76becceffa41e8f7cfc
Satar Sarafa <sp3c7r40x00@gmail.com>
2025-04-07 14:22:34 +0100

    Finished home tab

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image, FlatList, Pressable } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Colors } from '@/constants/color'
@@ -7,6 +7,7 @@ import HomeHeader from '@/components/HomeHeader'
 import FontSize from '@/constants/FontSize'
 import ProductTile from '@/components/ProductTile'
 import data from '@/data.json'
+import SearchBox from '@/components/SearchBox'
 // import {FlashList} from '@shopify/flash-list'
 const headerTiles = [
   { "id": "trade", "image": require("@/assets/images/trade.png") },
@@ -16,13 +17,18 @@ const headerTiles = [
   { "id": "rebuild", "image": require("@/assets/images/rebuild.png") }
 ]
 
+
 const home = () => {
+  const[searchText, setSearchText] = useState("")
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: "#111"}}>
-        <View style={{marginHorizontal: 16, marginTop: 10}}>
+      <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginHorizontal: 16, marginTop: 10}}>
+        <View style={{}}>
           <Text style={{color: "#fff", fontFamily: "Satoshi-Bold", fontSize: 19}}>Hello <Text style={{color: Colors.light.primary}}>Satar</Text></Text>
           <Text style={{color: "#fff", fontFamily: "Satoshi-Medium", fontSize: 10}}>{"Welcome to Greenloop"}</Text>
         </View>
+        <SearchBox value={searchText} onPress={() => console.log(searchText)} onChangeText={(value)=>setSearchText(value)}/>
+      </View>
       <View style={{ height: 120, backgroundColor: "#111"}}>
         {/* <HomeHeader/> */}
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -52,16 +58,35 @@ const home = () => {
           <Text style={{color: "#fff", fontFamily: "Satoshi-Medium", fontSize: 10, opacity: 0.2}}>Turn trash into cash the IDEAL way.</Text>
         </View>
         <View style={{ paddingTop: 10, marginHorizontal: 16 }}>
-          <Text style={{color: Colors.light.primary, fontFamily: "Satoshi-Medium", fontSize: 15}}>Products</Text>
-          <FlatList
+          <Text style={{color: Colors.light.primary, fontFamily: "Satoshi-Bold", fontSize: 18}}>Products</Text>
+           <FlatList
+            ListFooterComponent={<View style={{height: 300}}/>}
+            ListHeaderComponent={<View style={{height: 10}}/>}
+            data={data} // Assuming your data.json has a "products" array
+            keyExtractor={(item) => item.id.toString()} // Ensure each item has a unique key
+            numColumns={2} // Set the number of columns for the grid
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              paddingBottom: 20,
+            }}
+            columnWrapperStyle={{
+              justifyContent: "space-between", // Add spacing between columns
+              marginBottom: 5, // Add spacing between rows
+            }}
+            renderItem={({ item }) => (
+              <ProductTile data={item}/> // Pass the product data to ProductTile
+            )}
+          />
+          {/* <FlatList
             ListHeaderComponent={<View style={{height: 20}}/>}
             ListFooterComponent={<View style={{height: 270}}/>}
+            showsVerticalScrollIndicator={false}
             data={data} // Assuming your data.json has a "products" array
             renderItem={({ item }) => <ProductTile data={item} />} // Pass each product to ProductTile
             keyExtractor={(item) => item.id.toString()} // Ensure each item has a unique key
             // estimatedItemSize={100} // Provide an estimated size for better performance
             contentContainerStyle={{ paddingBottom: 20 }} // Add padding for better spacing
-          />
+          /> */}
         </View>
       </View>
       <StatusBar style="light" backgroundColor='#111'/>
--- components/ProductTile.tsx
+++ components/ProductTile.tsx
@@ -1,15 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
 
-const ProductTile = ({data}:{data: {name: string, date: string, time: string, location: string, image_url: string}}) => {
+interface DataProps {
+  id: number
+  name: string
+  date: string
+  time: string
+  location: string
+  image_url: string
+  content: string
+}
+const image = "https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWFuJTIwYXZhdGFyfGVufDB8fDB8fHww"
+
+const ProductTile = ({data}:{data: DataProps}) => {
   return (
-    <View style={{height: 100, backgroundColor: "#fff", borderRadius: 7, padding: 10, marginVertical: 5, flexDirection: "row", gap: 10}}>
-      <View style={{backgroundColor: "#333", height: "100%", width: 130, borderRadius: 7}}>
+    <View style={{width: "48%", padding: 10, height: 170, backgroundColor: "rgba(21, 232, 119,0.2)", borderRadius: 5, marginTop: 10, borderWidth: 1, borderColor: "rgba(21, 232, 119,0.5)"}}>
+      <Text style={{color: "#fff", fontFamily: "Satoshi-Bold", fontSize: 10}}>{data.name}</Text>
+      <Text style={{color: "#fff", fontFamily: "Satoshi-Medium", fontSize: 8, opacity: 0.5}}>{data.date +" "+ data.time}</Text>
+      <View style={{marginTop: 6, flexDirection: "row", justifyContent: "space-between"}}>
+        <Image source={{uri: data.image_url}} style={{ borderRadius: 6, width: 70, height: 70}}/>
+        <View style={{alignItems:"flex-end", gap: 3.5}}>
+          <Image source={{uri: image}} style={{ borderRadius: 6, width: 30, height: 30}}/>
+          <View style={{alignItems:"flex-end"}}>
+            <Text style={{fontFamily: "Satoshi-Bold", color: "#fff", fontSize: 8}}>Recycler</Text>
+            <Text style={{color: "#3CC687", fontFamily: "Satoshi-Regular", fontSize: 8}}>Spectra Gee</Text>
+          </View>
+          <View style={{flexDirection: "row", alignItems: "center", gap: 4}}>
+            <Image source={require("@/assets/images/location_1.png")} style={{width: 8, height: 8}} resizeMode='contain'/>
+            <Text style={{fontFamily: "Satoshi-Medium", fontSize: 10, color: "#fff"}}>{data.location.split(" ")[data.location.split(" ").length-1]}</Text>
+          </View>
+        </View>
       </View>
-      <Text style={{flex:1, width: "100%"}}>Name: {data.name}</Text>
-      <Text>Date: {data.date}</Text>
-      <Text>Name: Spectra Gee</Text>
-      <Text>Name: Spectra Gee</Text>
+        <Text style={{fontSize: 8.5, color: "#fff", position: "absolute", bottom: 10, left: 10, height: "25%"}}>{data.content.split(" ").slice(0, 20).join(" ")}...<Text style={{color: "#3CC687"}}>readmore</Text></Text>
     </View>
   )
 }
