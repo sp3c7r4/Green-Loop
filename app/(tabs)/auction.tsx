6ed9168870b89547e2a9822c6aea60fd616182ab
Satar Sarafa <sp3c7r40x00@gmail.com>
2025-04-13 20:14:51 +0100

    changes

--- app/(tabs)/auction.tsx
+++ app/(tabs)/auction.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import { color } from '@/constants/color';
 
 const auction = () => {
   return (
-    <View>
-      <Text>auction</Text>
-    </View>
-  )
+      <SafeAreaView style={{ flex: 1, backgroundColor: color.greensync.background, paddingHorizontal: 16 }} >
+        <StatusBar />
+      </SafeAreaView>
+  );
 }
 
 export default auction
